d35727cb15d0f6b594b3624f05bec24471e890a1
Monika2127 <47830785+Monika2127@users.noreply.github.com>
2025-01-28 19:38:58 +0530

    Add files via upload

---
+++ learn-investment-calculator/src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ms-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
